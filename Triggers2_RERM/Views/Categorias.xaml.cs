64a901640cb94906c480810fd8a07792091e32e5
Rochin04 <emirochi22@gmail.com>
2024-01-17 19:28:50 -0700

    Second commit

--- Triggers2_RERM/Views/Categorias.xaml.cs
+++ Triggers2_RERM/Views/Categorias.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Triggers2_RERM.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace Triggers2_RERM.Views
         public Categorias()
         {
             InitializeComponent();
+            BindingContext = new VMCategoris(Navigation);
         }
     }
 }
